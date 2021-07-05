ed8307bb43b71a8070ca6d1e7d9dad3b279520bf
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-07-05 22:42:29 +0530

    store user on registration in firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import Navbar from '@components/common/Navbar'
 import Footer from '@components/common/Footer'
+import { handleLogin } from '@lib/util'
 
-const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <Navbar />
+    <button onClick={handleLogin}>Sign in</button>
+    <Footer />
+  </div>
+)
 
 export default Home
