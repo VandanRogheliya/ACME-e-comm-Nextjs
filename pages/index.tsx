494329c6ccc392162ed73edbb3eb0cefe6e653d0
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-07-05 11:44:53 +0530

    made small change to test github workflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import Navbar from '@components/common/Navbar'
-import Footer from '@components/common/Footer'
+import Footerr from '@components/common/Footer'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Footer />
+      <Footerr />
     </div>
   )
 }
