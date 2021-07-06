083256ec9e68dc72ed7f2d766af119a2251e3642
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-07-06 22:44:20 +0530

    provide user context when logged in

---
+++ contexts/auth.tsx
@@ -0,0 +1,53 @@
+import { FIREBASE_COLLECTIONS } from '@lib/constants'
+import { firebaseAuth, firestore } from '@lib/firebase'
+import { UserType } from '@lib/types/common'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+type AuthContextValueType = {
+  user: UserType
+  isLoading: boolean
+}
+
+const AuthContext = createContext<AuthContextValueType>({
+  user: null,
+  isLoading: false,
+})
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState<UserType>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleCurrentUser = async () => {
+    setIsLoading(true)
+
+    try {
+      const currentUserUID = firebaseAuth.currentUser?.uid
+      if (currentUserUID) {
+        const userDoc = await firestore
+          .collection(FIREBASE_COLLECTIONS.USERS)
+          .doc(currentUserUID)
+          .get()
+        setUser(userDoc.data() as UserType)
+      } else setUser(null)
+    } catch (error) {
+      console.error(error)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    const unsubscribe = firebaseAuth.onAuthStateChanged(() =>
+      handleCurrentUser()
+    )
+    // Stop listening to auth state change on unmount
+    return () => unsubscribe()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading: isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const FIREBASE_COLLECTIONS = {
+  USERS: 'Users',
+  PRODUCTS: 'Products',
+}
