b45d199ef9daf7b021a71b0445ea82c96da7ca61
stevenovak123 <stevenovak0802@gmail.com>
2021-07-09 16:12:24 +0530

    added cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import Navbar from '@components/common/Navbar'
 import Footer from '@components/common/Footer'
+import Card from '@components/card/Card'
+import HomeCard from '@components/card/HomeCard'
+
+import { handleLogin } from '@lib/util'
 
 const Home = () => (
   <div>
     <Navbar />
+    <HomeCard />
+
+    <button onClick={handleLogin}>Sign in</button>
+    <Card />
     <Footer />
   </div>
 )
