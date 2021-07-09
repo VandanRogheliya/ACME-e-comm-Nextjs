ebcf753d035879e48cdf340a10d36d29d82455c8
stevenovak123 <stevenovak0802@gmail.com>
2021-07-09 21:11:09 +0530

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,11 @@ import Footer from '@components/common/Footer'
 import Card from '@components/card/Card'
 import HomeCard from '@components/card/HomeCard'
 
-import { handleLogin } from '@lib/util'
 
 const Home = () => (
   <div>
     <Navbar />
     <HomeCard />
-
-    <button onClick={handleLogin}>Sign in</button>
     <Card />
     <Footer />
   </div>
