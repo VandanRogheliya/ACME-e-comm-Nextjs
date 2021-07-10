9a58e7a61a90f2beaf69050c1ff4f215b4273991
stevenovak123 <stevenovak0802@gmail.com>
2021-07-10 15:04:51 +0530

    Card updated

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,38 +1,19 @@
+
+
 const Card = () => {
   return (
-    <div className="flex flex-row lg:flex-row-2 text-white">
+    <div className=" max-w-6xl mx-auto flex flex-row lg:flex-row-2 text-white">
       <div className="group bg-black ">
-        <div className="flex flex-col flex-initial items-start">
-          <div className="text-white p-0 bg-black group-hover:bg-purple-600">
-            Clothing
+        <div className=" flex flex-col flex-initial items-start">
+          <h3 className="font-bold text-2xl text-white p-2 bg-black group-hover:bg-purple-600">
+            <span>Clothing </span>
+          </h3>
+          <div className="text-white p-4 font-bold text-xl group-hover:bg-purple-600 inline-block">
+            {' '}
+            $50
           </div>
-          <div className="text-white p-0 ">
-            <span className="bg-black px-1 pt-0 group-hover:bg-purple-600 group-hover:border group-hover:border-purple-600 ">
-              $50
-            </span>
-          </div>
           <img
-            className="w-100 h-auto"
-            src="https://cdn11.bigcommerce.com/s-qfzerv205w/images/stencil/original/products/115/489/Hat-front-black__72990.1603748583.png"
-          ></img>
-        </div>
-      </div>
-      <div className="group bg-black ">
-        <div className="flex flex-col flex-initial group-hover:bg-purple-600">
-          <div className="text-white p-0">Clothing</div>
-          <div className="text-white p-0">$50</div>
-          <img
-            className="w-100 h-auto"
-            src="https://cdn11.bigcommerce.com/s-qfzerv205w/images/stencil/original/products/115/489/Hat-front-black__72990.1603748583.png"
-          ></img>
-        </div>
-      </div>
-      <div className="group bg-black ">
-        <div className="flex flex-col flex-initial group-hover:bg-purple-600">
-          <div className="text-white p-0">Clothing</div>
-          <div className="text-white p-0">$50</div>
-          <img
-            className="w-100 h-auto"
+            className="w-11/12 mx-auto h-auto group-hover:scale-150"
             src="https://cdn11.bigcommerce.com/s-qfzerv205w/images/stencil/original/products/115/489/Hat-front-black__72990.1603748583.png"
           ></img>
         </div>
