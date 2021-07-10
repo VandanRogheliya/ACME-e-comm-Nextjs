d5469a44874d2a8fc23a3860ad90f519ab55c5d0
stevenovak123 <stevenovak0802@gmail.com>
2021-07-10 22:45:27 +0530

    Updated nav & Homepage

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,8 +1,6 @@
-
-
 const Card = () => {
   return (
-    <div className=" max-w-6xl mx-auto flex flex-row lg:flex-row-2 text-white">
+    <div className=" w-1/3 max-w-6xl mx-auto flex flex-row lg:flex-row-2 text-white">
       <div className="group bg-black ">
         <div className=" flex flex-col flex-initial items-start">
           <h3 className="font-bold text-2xl text-white p-2 bg-black group-hover:bg-purple-600">
@@ -12,14 +10,15 @@ const Card = () => {
             {' '}
             $50
           </div>
-          <img
-            className="w-11/12 mx-auto h-auto group-hover:scale-150"
-            src="https://cdn11.bigcommerce.com/s-qfzerv205w/images/stencil/original/products/115/489/Hat-front-black__72990.1603748583.png"
-          ></img>
+          <div className="w-10/12 mx-auto h-auto hover:w-11/12 cursor-pointer">
+            <img
+              className=""
+              src="https://cdn11.bigcommerce.com/s-qfzerv205w/images/stencil/original/products/115/489/Hat-front-black__72990.1603748583.png"
+            ></img>
+          </div>
         </div>
       </div>
     </div>
   )
 }
-
 export default Card
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import Navbar from '@components/common/Navbar'
 import Footer from '@components/common/Footer'
 import Card from '@components/card/Card'
 import HomeCard from '@components/card/HomeCard'
-
+import HomeCardHero from '@components/card/HomeCardHero'
 
 const Home = () => (
   <div>
     <Navbar />
-    <HomeCard />
+    <div className="flex  flex-col flex-intial lg:flex-row">
+      <div className="grid lg:grid-cols-7">
+        <div className="lg:col-span-5 lg:row-span-4">
+          <HomeCardHero />
+        </div>
+        <div className="lg:row-span-3 lg:col-span-2">
+          <HomeCard />
+        </div>
+      </div>
+    </div>
     <Card />
     <Footer />
   </div>
