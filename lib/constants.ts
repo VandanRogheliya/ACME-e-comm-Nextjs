983596aa04eda955f3058bda36c85a6eaa1586d0
stevenovak123 <stevenovak0802@gmail.com>
2021-07-13 15:55:04 +0530

    pass props to product card

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,18 @@ export const FIREBASE_COLLECTIONS = {
   CART_ITEM: 'CartItem',
   ORDER_ITEM: 'OrderItem',
 }
+export const PID_ARRAY = [
+  '115',
+  '116',
+  '117',
+  '118',
+  '119',
+  '120',
+  '121',
+  '122',
+  '123',
+  '124',
+  '125',
+  '126',
+  '127',
+]
