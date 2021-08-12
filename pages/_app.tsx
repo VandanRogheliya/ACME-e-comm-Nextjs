0242f7a36cebdbc9ba9df424d3ac4af0aa5bc519
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-08-12 22:46:57 +0530

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import '@styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from 'contexts/auth'
 import { CartSidebarProvider } from 'contexts/cartSidebar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <CartSidebarProvider>
-        <Component {...pageProps} />
-      </CartSidebarProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <title>ACME: Clothing Store</title>
+      </Head>
+      <AuthProvider>
+        <CartSidebarProvider>
+          <Component {...pageProps} />
+        </CartSidebarProvider>
+      </AuthProvider>
+    </>
   )
 }
 export default MyApp
